9b6bbfbd06805a44418f6a72aeeb412ee172028d
GloRRian55 <GloRRian55@yandex.ru>
2024-12-06 22:16:22 +0300

    fix cmake build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "2.1.0"
+    id("io.github.glorrian.cmake-gradle-plugin") version "1.0.1"
 }
 
 group = "io.github.glorrian"
@@ -14,6 +15,10 @@ dependencies {
     implementation("org.openpnp:opencv:4.9.0-0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+cmake {
+    arguments = listOf("-DLIBRARY_PATH=../../src/main/resources")
 }
 
 tasks.test {
